769eb41cca281c5606bbc2cf689b50d629b810a3
Pasha Smolov <palexsmol@gmail.com>
2019-09-24 14:56:07 -0700

    add listing note component

---
+++ src/components/Listing/Note.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { TNote } from "../../reducers/notes/types"
+
+interface Props {
+  note: TNote
+}
+
+const Note: React.FC<Props> = ({ note }) => {
+  return <div dangerouslySetInnerHTML={{ __html: note.body }}></div>
+}
+
+export default Note
