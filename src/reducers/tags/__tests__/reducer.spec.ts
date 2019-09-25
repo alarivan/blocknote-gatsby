e9684b6b3098462bdafb446c63c3f8ede27bcbfc
Pasha Smolov <palexsmol@gmail.com>
2019-09-25 13:52:02 -0700

    add tags reducer

---
+++ src/reducers/tags/__tests__/reducer.spec.ts
@@ -0,0 +1,36 @@
+import { tagsReducer } from "../reducer"
+import { emptyAction } from "../../actions"
+import { setTagsAction, saveTagAction, deleteTagAction } from "../actions"
+import { Map } from "immutable"
+
+describe("tagsReducer", () => {
+  it("returns initials state", () => {
+    const state = tagsReducer(Map(), emptyAction())
+
+    expect(state).toEqual(Map())
+  })
+
+  it("sets tags", () => {
+    const tags = Map([["fake_id", { id: "fake_id", color: "black&white" }]])
+    const action = setTagsAction(tags)
+    const state = tagsReducer(Map(), action)
+
+    expect(state).toEqual(tags)
+  })
+
+  it("adds tag", () => {
+    const tag = { id: "fake_id", color: "black&white" }
+    const action = saveTagAction(tag)
+    const state = tagsReducer(Map(), action)
+
+    expect(state).toEqual(Map([[tag.id, tag]]))
+  })
+
+  it("delete tag", () => {
+    const tag = { id: "fake_id", color: "black&white" }
+    const action = deleteTagAction(tag)
+    const state = tagsReducer(Map(), action)
+
+    expect(state).toEqual(Map())
+  })
+})
---
+++ src/reducers/tags/types.ts
@@ -0,0 +1,37 @@
+import { EMPTY_ACTION } from "../types"
+import { Map } from "immutable"
+
+export type TTag = {
+  id: string
+  color: string
+}
+export type TTagsState = Map<string, TTag>
+
+export const SET_TAGS = "SET_TAGS"
+export type SET_TAGS_ACTION = {
+  type: typeof SET_TAGS
+  payload: {
+    tags: TTagsState
+  }
+}
+export const SAVE_TAG = "SAVE_TAG"
+export type SAVE_TAG_ACTION = {
+  type: typeof SAVE_TAG
+  payload: {
+    tag: TTag
+  }
+}
+
+export const DELETE_TAG = "DELETE_TAG"
+export type DELETE_TAG_ACTION = {
+  type: typeof DELETE_TAG
+  payload: {
+    tag: TTag
+  }
+}
+
+export type TTagsAction =
+  | SET_TAGS_ACTION
+  | SAVE_TAG_ACTION
+  | DELETE_TAG_ACTION
+  | EMPTY_ACTION
