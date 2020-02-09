70b9664579626c6e3409f6121c4eb980c40fd079
alarivan <alarivan@pm.me>
2020-02-08 18:41:04 -0800

    Switch to material-ui

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,39 +1,44 @@
 import { Link } from "gatsby"
 import React from "react"
-import styled from "@emotion/styled"
+import { makeStyles } from "@material-ui/core/styles"
 
 type Props = {
   siteTitle: string
 }
 
-const StyledHeader = styled.header`
-  background: rebeccapurple;
-  margin-bottom: 1.45rem;
-`
+const useStyles = makeStyles({
+  header: {
+    background: "rebeccapurple",
+    marginBottom: "1.45.rem",
+  },
+  inner: {
+    margin: "0 auto",
+    maxWidth: "960px",
+    padding: "1.45rem 1.0875rem",
+  },
+  h1: {
+    margin: "0 auto",
+  },
+  link: {
+    color: "white",
+    textDecoration: "none",
+  },
+})
 
-const StyledHeaderInner = styled.div`
-  margin: 0 auto;
-  max-width: 960px;
-  padding: 1.45rem 1.0875rem;
-`
+const Header: React.FC<Props> = ({ siteTitle }) => {
+  const classes = useStyles()
 
-const StyledHeaderH1 = styled.h1`
-  margin: 0 auto;
-`
-
-const StyledHeaderLink = styled(Link)`
-  color: white;
-  text-decoration: none;
-`
-
-const Header: React.FC<Props> = ({ siteTitle }) => (
-  <StyledHeader>
-    <StyledHeaderInner>
-      <StyledHeaderH1>
-        <StyledHeaderLink to="/">{siteTitle}</StyledHeaderLink>
-      </StyledHeaderH1>
-    </StyledHeaderInner>
-  </StyledHeader>
-)
+  return (
+    <header className={classes.header}>
+      <div className={classes.inner}>
+        <h1 className={classes.h1}>
+          <Link className={classes.link} to="/">
+            {siteTitle}
+          </Link>
+        </h1>
+      </div>
+    </header>
+  )
+}
 
 export default Header
