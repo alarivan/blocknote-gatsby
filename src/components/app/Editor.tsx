a394ae9c2e5cfa7e159e9bf9335f7cc5dd2bdc9f
alarivan <alarivan@pm.me>
2020-04-01 17:33:41 -0700

    Add TagsInput to Editor

--- src/components/app/Editor.tsx
+++ src/components/app/Editor.tsx
@@ -1,9 +1,11 @@
 import React, { useState, Dispatch, useEffect } from "react"
+import { navigate } from "gatsby"
 import ReactQuill from "react-quill"
 import "react-quill/dist/quill.snow.css"
 import { TNote, TNotesAction } from "../../reducers/notes/types"
 import { saveNoteAction } from "../../reducers/notes/actions"
-import { navigate } from "gatsby"
+import TagsInput from "./common/TagsInput"
+import useNoteTags from "../../hooks/useNoteTags"
 
 interface Props {
   note: TNote
@@ -12,6 +14,7 @@ interface Props {
 
 const Editor: React.FC<Props> = ({ note, dispatch }) => {
   const [text, setText] = useState(note.body)
+  const noteTags = useNoteTags(note.tags)
 
   useEffect(() => {
     setText(note.body)
@@ -19,7 +22,14 @@ const Editor: React.FC<Props> = ({ note, dispatch }) => {
 
   const handleChange = (value: string) => setText(value)
   const handleSave = () => {
-    dispatch(saveNoteAction({ id: note.id, body: text }))
+    dispatch(saveNoteAction({ id: note.id, body: text, tags: note.tags }))
+
+    navigate(`app/edit/${note.id}`)
+  }
+
+  const handleTagAdd = (tag: string) => {
+    const newTags = note.tags.add(tag)
+    dispatch(saveNoteAction({ id: note.id, body: text, tags: newTags }))
 
     navigate(`app/edit/${note.id}`)
   }
@@ -27,6 +37,7 @@ const Editor: React.FC<Props> = ({ note, dispatch }) => {
   return (
     <>
       <ReactQuill value={text} onChange={handleChange} />
+      <TagsInput noteTags={noteTags} onTagAdd={handleTagAdd} />
       <button onClick={handleSave}>Save</button>
     </>
   )
---
+++ src/hooks/useNoteTags.ts
@@ -0,0 +1,15 @@
+import { useContext, useMemo } from "react"
+import { Seq } from "immutable"
+import { TTag } from "./../reducers/tags/types"
+import { TNoteTags } from "../reducers/notes/types"
+import { TagsContext } from "./../context/TagsContext"
+
+export default function useNoteTags(tags: TNoteTags): Seq.Indexed<TTag> {
+  const { state } = useContext(TagsContext)
+  const tagItems = useMemo(
+    () => (tags.map(tag => state.get(tag)) as unknown) as TTag[],
+    [tags]
+  )
+
+  return Seq.Indexed(tagItems)
+}
